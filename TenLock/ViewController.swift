f7ba46b874340e7a37e911c67612508e95887c9a
zmh <739296759@qq.com>
2019-02-20 16:12:02 +0800

    Initial Commit

---
+++ TenLock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TenLock
+//
+//  Created by iOS123 on 2019/2/20.
+//  Copyright © 2019年 iOS123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
